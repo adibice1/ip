06ee8eb8ddd14fa903de9f8ce87291bfa8d5328a
adib <adibice1@gmail.com>
2025-08-22 20:53:03 +0800

    level 1

--- src/main/java/Dibo.java
+++ src/main/java/Dibo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Dibo {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,11 +7,24 @@ public class Dibo {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("-------------------------------------------------\n");
+        Scanner scanner = new Scanner(System.in);
+        String horizontalLine = "===============================================";
+
+        System.out.println(horizontalLine);
         System.out.println("Hello! I'm Dibo the Dragon\n");
         System.out.println("What can I do for you?\n");
-        System.out.println("-------------------------------------------------\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("-------------------------------------------------");
+        System.out.println(horizontalLine);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            System.out.println(horizontalLine);
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
+                break;
+            }
+            System.out.println(userInput);
+            System.out.println(horizontalLine);
+        }
     }
 }
